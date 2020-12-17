2602588a2351b6c752605abb3a3bd0780519fcdd
Samantha Goodman <antashma@gmail.com>
2020-12-16 20:02:53 -0500

    add helmet and cors dependencies

--- api/server.js
+++ api/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
 const server = express();
 
 server.use(express.json());
+server.use(helmet());
+server.use(cors());
 
 server.get('', (req, res)=> {
     res.send({api: 'up'})
