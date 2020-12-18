912aeb00c97735f18fbde224ad71b0e3edf92513
Samantha Goodman <antashma@gmail.com>
2020-12-17 21:44:34 -0500

    update db-config for heroku

--- database/db-config.js
+++ database/db-config.js
@@ -2,7 +2,7 @@ const knex = require('knex');
 const config = require('../knexfile.js');
 const dotenv = require('dotenv').config();
 
-const db = knex(config[process.env.KNEX_ENV]);
+const db = knex(config[process.env.NODE_ENV]);
 
 
 
